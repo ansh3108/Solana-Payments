21188e9e65507493e8867a3abcb216756f0c7769
ansh3108 <anshkumar9686@gmail.com>
2025-09-16 15:50:16 +0530

    tailwindcss issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-export default function App() {
+function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-purple-600 text-white text-4xl font-bold">
-      🎉 Tailwind is Working!
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-blue-500">
+      <h1 className="text-5xl font-bold text-white">
+        Tailwind is Working 🎉
+      </h1>
     </div>
   );
 }
+
+export default App;
